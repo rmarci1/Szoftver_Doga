e4e12d1f4297979972245075e210d76c8a8c656f
Marcell Rabotka <rigbimarci@gmail.com>
2023-12-18 12:45:48 +0100

    3.feladat lista kiíratása vesszővel elválasztva

--- szoft_Doga/Program.cs
+++ szoft_Doga/Program.cs
@@ -14,6 +14,11 @@ namespace szoft_Doga
             List<int> list = new List<int>();
             while (szam != 0)
             {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.Write($"{list[i]},");
+                }
+                Console.WriteLine();
                 Console.WriteLine("Kérek egy számot, Vége:0");
                 szam = Convert.ToInt32(Console.ReadLine());
                 if (szam != 0)
